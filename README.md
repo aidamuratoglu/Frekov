584f5c1955ea040ef36f619b5b0dd68f8a4b2ba6
Aida Muratoglu <aidamuratogu98@gmail.com>
2021-02-28 20:51:10 -0500

    the rest of the project

--- README.md
+++ README.md
@@ -6,26 +6,39 @@ her starts with this Julia Kristeva quote from *Giotto's Joy*: "How can we find 
 through what separates words from what is both without a name and more than a name: 
 a painting? What is it that we are trying to go through? The space of the very act of naming?"
 I find it personally meaningful in the context of this project, as well. Parsing Frecon's
-paintings into backgrounds and shapes, assigning numbers to them, and manipulating them
+paintings into backgrounds and shapes, assigning letters to them, and manipulating them
 at will has added another level to Kristeva's initial inquiry. How can we find our way
 through what separates strings from reality? On a more immediate level, what does it mean
-to assign numbers--totally changing the naming structure and its implicit meaning--to shapes?
+to assign letters--totally changing the naming structure and its implicit meaning--to shapes?
 Does it take away from their power as objects that exist within the built world of a painting?
 Frecon is known for her expansive color palette, large canvas size, and her many repeating
 shapes. When I first learned about this project, my mind immediately jumped to her work. How
 could I create a program inspired by her paintings, these somewhat iterative, gorgeous works
 of art? 
 
-This project has been very challenging and quite rewarding. I have learend a *lot* of 
-python in a relatively short period of time (less than a week), reverse engineered a program,
+This project has been very challenging and quite rewarding. I have learned a *lot* of 
+python in a relatively short period of time (about a week), reverse engineered a program,
 and spent many hours debugging. Most importantly, I have spent many hours thinking about what
-computional creativity looks like. 
+computional creativity looks like. I learned about ints, tuples, dictionaries, debugged a lot,
+learned about docstring and other python style guides. I pushed myself outside my comfort zone
+by taking on a project that was much more intense than I was expecting. 
 
 For a future project, I would like to create a program that generates titles for these
 new Frekov images. For instance, frecon-1 (in assets) is called "mars indigo." I would also
 like to work on creating a program that generates its own transition matrix (rather than a
-hard-coded one).
+hard-coded one). I would also like to figure out a way to take out the white space around the 
+shapes, use random to create f-strings that call different backgrounds (listed in assets as
+background_1.jpg, etc.). It would look something like this: f"background_{random.randint(1,3)}". 
+I think I can do this with treating the global variables of background colors as tuples. For a
+future day!
+
 
 I would say this system is relatively creative. It's not as *innovative* as I'd like it to be,
 but I think it's pretty novel. Looking back over my code, I realize how closely I followed
-the markov_musician example Prof. Harmon offered for the class. 
+the markov_musician example Prof. Harmon offered for the class. Still, I think this code is
+creative in its application of shapes and backgrounds - quite literally, the program creates
+collages based on an existing artist's work. I also think the code is creative in the ways it
+uses space. 
+
+Sources: I used some posts on Stack Overflow, Kim Hancock talked through some of the earlier 
+parts of the project with me, and I'd like to thank my roommates for putting up with me this week.
--- frekov-painter.py
+++ frekov-painter.py
@@ -8,24 +8,25 @@ Dependencies: numpy, PIL
 from PIL import Image
 import numpy as np
 
+# Potential colors to hard-code into line 89 to change background color.
+# Colors derived from running a getpixel() command on input backgrounds.
+BACKGROUND_COLORS = {
+    "background_1": (10, 84, 119),
+    "background_2": (58, 41, 59),
+    "background_3": (94, 45, 40)
+}
+
 class Frekov:
-    def __init__(self):
+    def __init__(self, transition_matrix):
         """Simulates a painter that relies on a simple Markov chain.
             Args:
                 transition_matrix (dict): list of shapes and their 
                     probabilites based on their respective paintings.
         """
-        self.transition_matrix = {
-            "a": {"a": 0.2, "b": 0.7, "c": 0.25, "d": 0.25, "e": 0.25, "f": 0.25},
-            "b": {"a": 0.7, "b": 0.2, "c": 0.25, "d": 0.25, "e": 0.25, "f": 0.25},
-            "c": {"a": 0.02, "b": 0.02, "c": 0.02, "d": 0.9, "e": 0.02, "f": 0.02},
-            "d": {"a": 0.9, "b": 0.02, "c": 0.02, "d": 0, "e": 0.02, "f": 0.02},
-            "e": {"a": 0.1, "b": 0.2, "c": 0.1, "d": 0.1, "e": 0.3, "f": 0.2},
-            "f": {"a": 0.2, "b": 0.1, "c": 0.2, "d": 0.1, "e": 0.1, "f": 0.3}
-        }
+        self.transition_matrix = transition_matrix
         self.shape_names = list(self.transition_matrix.keys())
     
-    def create_components(self, current_shape="1", shape_count=3):
+    def create_components(self, current_shape="a", shape_count=3):
         """Generates a list of shapes to be added to a painting.  
             Args:
                 current_shape (str): current shape being added to painting.
@@ -42,8 +43,6 @@ class Frekov:
             components.append(next_shape)
             current_shape = next_shape
         
-        print(*components, sep = ", ")
-
         return components
 
     def get_next_shape(self, current_shape):
@@ -53,20 +52,14 @@ class Frekov:
             Returns:
                 a string representing the next shape to add.
         """
+        probabilities = []
+        
+        for next_shape in self.shape_names:
+            probabilities.append(self.transition_matrix[current_shape][next_shape])
+               
         return np.random.choice(
-            self.shape_names,
-            p=[self.transition_matrix[current_shape][next_shape] \
-            for next_shape in self.shape_names]
-        )    
-
-    def get_background(self):
-        """Chooses random background for new painting based on a selection of 
-            3 backgrounds from Frecon paintings. 
-            
-            Returns: 
-                background (string): name of background file to call.
-        """
-        return f"background_{np.random.randint(1,3)}"
+            self.shape_names, p=probabilities
+        )
 
     def turn_to_shape(self, components):
         """Creates a list of shapes (Images) based on the following args:    
@@ -78,14 +71,15 @@ class Frekov:
                 shapes (list): list of shapes (as Images) to use in painting. 
         """
         shapes = []
-        for i in components:
-            im = Image.open(f"{components[i-1]}.jpg")
+
+        for shape in components:
+            im = Image.open(f"{shape}.jpg")
             shapes.append(im)
 
         return shapes
     
     def make_painting(self, components):
-        """Creates a collage consisting of the chosen background and given shapes.
+        """Creates a collage consisting of a background and given shapes.
             Args:
                 shapes (list): list of shapes (as Images). 
 
@@ -93,24 +87,33 @@ class Frekov:
                 painting (Image): new painting with background and new shapes.
         """
         shape_data = self.turn_to_shape(components)
-        painting = Image.new(self.get_background, (1400, 1725))
-        height = painting.size
-        thumbnail_height = height//len(shape_data)
+        painting = Image.new("RGB", (1600, 1925), (58, 41, 59))
+        height, width = painting.size
+        print("height: ", height)
+        int(height)
+        thumbnail_height = int(height)//len(shape_data)
         i = 0
         x = 0
         y = 0
         while i < len(shape_data):
-            print(i, x, y)
             painting.paste(shape_data[i], (x,y))
             i += 1
+            x += 1
             y += thumbnail_height
         painting.save("new-painting.jpg")
 
 
 def main():
-    painting_maker = Frekov()
+    painting_maker = Frekov({
+        "a": {"a": 0.2, "b": 0.7, "c": 0.025, "d": 0.025, "e": 0.025, "f": 0.025},
+        "b": {"a": 0.7, "b": 0.2, "c": 0.025, "d": 0.025, "e": 0.025, "f": 0.025},
+        "c": {"a": 0.02, "b": 0.02, "c": 0.02, "d": 0.9, "e": 0.02, "f": 0.02},
+        "d": {"a": 0.9, "b": 0.02, "c": 0.02, "d": 0.02, "e": 0.02, "f": 0.02},
+        "e": {"a": 0.1, "b": 0.2, "c": 0.1, "d": 0.1, "e": 0.3, "f": 0.2},
+        "f": {"a": 0.2, "b": 0.1, "c": 0.2, "d": 0.1, "e": 0.1, "f": 0.3}
+    })
 
-    new_painting = painting_maker.create_components(current_shape="1", \
+    new_painting = painting_maker.create_components(current_shape="d", \
         shape_count=2)
 
     print("Making next painting....")
